d117f913578bc02df8a28db15a6311d7a99de2ab
Valerij <115507066+Valerij-Hering@users.noreply.github.com>
2023-03-29 14:48:15 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+require('dotenv').config();
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+mongoose.set("strictQuery", false);
+
+const PORT = 8000 || process.env.port;
+
+mongoose
+.connect(process.env.MONGODB_LINK)
+.then(() => console.log('WE WERE CONNECTED TO MONGO'))
+.catch((err) => console.log(err))
+
+const messageSchema = {
+    name: String,
+    email: String,
+    message: String
+}
+
+const Message = mongoose.model('MyMessages', messageSchema)
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.post('/', (req, res) => {
+    let newMessage = new Message({
+        name: req.body.name,
+        email: req.body.email,
+        message: req.body.message,
+    })
+    newMessage.save()
+    res.sendFile(__dirname + "/answer.html")
+})
+
+app.listen(PORT, () => {
+    console.log(`I AM LISTENNING ON PORT ${PORT}`)
+})
