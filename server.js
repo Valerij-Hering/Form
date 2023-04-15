659acb54ce0e0402aaea7e59181f5ba2550f26ba
Valerij <115507066+Valerij-Hering@users.noreply.github.com>
2023-04-15 18:32:12 +0200

    Update server.js

--- server.js
+++ server.js
@@ -24,11 +24,11 @@ const messageSchema = {
 
 const Message = mongoose.model('MyMessages', messageSchema)
 
-app.get('https://form-vh.netlify.app/', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html")
 })
 
-app.post('https://form-vh.netlify.app/', (req, res) => {
+app.post('/', (req, res) => {
     let newMessage = new Message({
         name: req.body.name,
         email: req.body.email,
